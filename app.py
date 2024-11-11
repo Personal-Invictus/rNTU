9bdcc74a9411bec8e20ecd6aa4bcfda929489c38
chenshigong <70263153+rockchenshigong@users.noreply.github.com>
2024-11-11 12:37:28 +0000

    sychronize

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 from flask import Flask
+from flask import render_template,request
+
+app = Flask("__name__") #功能署名
+@app.route("/",methods=['Get','POST'])
+def index():
+    return(render_template('index.html'))
+
+if __name__ == '__main__': #下划线是系统预留的
+    app.run()
