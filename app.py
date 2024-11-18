b00f2b3eda6a8214a48a222fc691ec1776051f6b
chenshigong <70263153+rockchenshigong@users.noreply.github.com>
2024-11-18 12:19:02 +0000

    1

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask("__name__") #功能署名
 def index():
     return(render_template('index.html'))
 
+@app.route("/main",methods=['Get','POST'])
+def index():
+    name = request.form.get("q")
+    return(render_template('main.html'))
+
 if __name__ == '__main__': #下划线是系统预留的
     app.run()
